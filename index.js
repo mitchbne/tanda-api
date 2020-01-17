621b801ca288318286ab18727d824a4bac6a9e11
Mitch Smith <mitchpsmith1998@gmail.com>
2020-01-17 14:49:00 +1000

    add eslint for code quality

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+const fs = require("fs")
 const axios = require("axios")
 const Papa = require("papaparse")
-const fs = require("fs")
 require("dotenv").config() // Load in Environmental variables.
 
 // Editable values
@@ -10,42 +10,42 @@ const API_ENDPOINT = process.env.API_ENDPOINT
 
 // Setup default API connection configuration
 axios.defaults.baseURL = "https://my.tanda.co/api/v2/"
-axios.defaults.headers.common['Authorization'] = `Bearer ${AUTH_TOKEN}`
-axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.headers.common["Authorization"] = `Bearer ${AUTH_TOKEN}`
+axios.defaults.headers.post["Content-Type"] = "application/json"
 
 fs.readFile("./data.csv", "utf-8", (err, data) => {
-  if (err) throw err
+  if (err) {throw err}
   else {
     Papa.parse(data, {
       dynamicTyping: true,
       header: true,
       complete: ({ data: dataRows }) => {
-        var errors = []
-        var newEntries = []
+        const errors = []
+        const newEntries = []
         Promise.all(
-          dataRows.map((row, rowIndex) => new Promise((resolve, reject) => {
+          dataRows.map((row, rowIndex) => new Promise((resolve) => {
             axios.post(API_ENDPOINT, { ...row })
-            .then(({ status, statusText, data }) => {
-              if (status >= 200 && status <= 205){
-                newEntries.push({ id: data.id })
-                console.log("✅ Created new entry", data.id )
-              }
-            })
-            .catch((error) => {
-              errors.push({ error, row })
-              console.log("❌ Error creating entry. CSV row ", rowIndex + 1)
-            })
-            .finally(() => { resolve() })
+              .then(({ status, data }) => {
+                if (status >= 200 && status <= 205){
+                  newEntries.push({ id: data.id })
+                  console.log("✅ Created new entry", data.id )
+                }
+              })
+              .catch((error) => {
+                errors.push({ error, row })
+                console.log("❌ Error creating entry. CSV row ", rowIndex + 1)
+              })
+              .finally(() => { resolve() })
           }))
         ).then(() => {
           if (errors.length > 0){
-            errors.forEach(e => {
+            errors.forEach((e) => {
               console.log(e.error)
             })
-            output_rows = errors.map(line => line.row)
-            csv = Papa.unparse(output_rows)
-            fs.writeFile('rows_not_sent.csv', csv, (err) => {
-              if (err) throw err
+            const output_rows = errors.map(line => line.row)
+            const csv = Papa.unparse(output_rows)
+            fs.writeFile("rows_not_sent.csv", csv, (err) => {
+              if (err) {throw err}
               console.log("Rows that did not get created have been output to 'rows_not_sent.csv' ")
             })
           }
@@ -54,11 +54,11 @@ fs.readFile("./data.csv", "utf-8", (err, data) => {
           console.log(`${newEntries.length} rows created.`)
           console.log(`${errors.length} errors occured.\n\n`)
         })
-        .catch((err) => {
-          console.log("It semes there was some dodgy CSV stuff parsed.")
-          console.log(err)
-        })
-      }
+          .catch((err) => {
+            console.log("It semes there was some dodgy CSV stuff parsed.")
+            console.log(err)
+          })
+      },
     })
   }
 })
